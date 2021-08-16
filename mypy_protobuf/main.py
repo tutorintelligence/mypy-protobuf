ff1271bcf52ee33f4d64273101a1d567fe2a1482
Jack Strang <jack@tutorintelligence.com>
2021-08-16 17:51:46 -0400

    return awaitables

--- mypy_protobuf/main.py
+++ mypy_protobuf/main.py
@@ -683,7 +683,13 @@ class PkgWriter(object):
     def _output_type(
         self, method: d.MethodDescriptorProto, use_stream_iterator: bool = True
     ) -> str:
-        result = self._import_message(method.output_type)
+        result = "{}[{}, {}[{}]]".format(
+            self._import("typing", "Union"),
+            self._import_message(method.output_type),
+            self._import("typing", "Awaitable"),
+            self._import_message(method.output_type),
+        ),
+        result = self._import_message(method.output_type) # TODO
         if use_stream_iterator and method.server_streaming:
             result = "{}[{}]".format(self._import("typing", "Iterator"), result)
         return result
